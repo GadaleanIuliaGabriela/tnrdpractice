e9381485bb430001aea55aa095cd27a608ca6b12
Iulia Gadalean <gadalean.iulia.gabriela@gmail.com>
2021-11-11 17:24:33 +0200

    Add express.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,4 +1,21 @@
+import express, {RequestHandler, Request, Response, NextFunction} from 'express';
+import {json} from 'body-parser';
 import {concat} from 'lodash';
 
+const app = express();
+
+app.use(json());
+
+const getMessage: RequestHandler = (req, res, next) => {
+  res.json({
+    message: "Here is your message.",
+    numbers: concat([1, 2, 3], [10, 11, 12])
+  })
+}
+
+app.get('/', getMessage);
+
 console.log('Hello World!');
-console.log(concat([1,2,3], [10,11,12]));
+console.log(concat([1, 2, 3], [10, 11, 12]));
+
+app.listen(3000)
