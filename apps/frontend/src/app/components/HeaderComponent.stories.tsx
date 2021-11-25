9c9b71c9a9c9d386304ddad1e9f74a45d55263ed
Iulia Gadalean <gadalean.iulia.gabriela@gmail.com>
2021-11-25 14:40:35 +0200

    Fix HeaderComponent in storybook.

--- apps/frontend/src/app/components/HeaderComponent.stories.tsx
+++ apps/frontend/src/app/components/HeaderComponent.stories.tsx
@@ -1,13 +1,14 @@
 import * as React from "react";
 import { Story, Meta } from '@storybook/react';
 import HeaderComponent from './HeaderComponent';
+import { MemoryRouter } from "react-router";
 
 export default {
   component: HeaderComponent,
   title: 'HeaderComponent',
 } as Meta;
 
-const Template: Story = (args) => <HeaderComponent title={"Test"} {...args} />;
+const Template: Story = (args) => <MemoryRouter><HeaderComponent title={"Test"} {...args} /></MemoryRouter>;
 
 export const Primary = Template.bind({});
 Primary.args = {};
